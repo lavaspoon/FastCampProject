5a860a71099ab4bbbc5d09b97972573b618091b3
lavaspoon <lavaspoon@naver.com>
2021-10-04 23:21:19 +0900

    UI 반영되도록 수정

--- FastCampProject/FastCampProject/CodePresentViewController.swift
+++ FastCampProject/FastCampProject/CodePresentViewController.swift
@@ -14,7 +14,10 @@ class CodePresentViewController: UIViewController {
     //ViewController.swift에서 name 프로퍼티에 접근하기 위해서는 뷰컨트롤러에서 다운캐스팅 해야함
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //옵셔널이기 떄문에 옵셔널 바인딩
+        if let name = name {
+            self.nameLabel.text = name
+        }
     }
     @IBAction func backBtn(_ sender: UIButton) {
         presentingViewController?.dismiss(animated: true, completion: nil)
--- FastCampProject/FastCampProject/CodePushViewController.swift
+++ FastCampProject/FastCampProject/CodePushViewController.swift
@@ -14,6 +14,10 @@ class CodePushViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //옵셔널이기 떄문에 옵셔널 바인딩
+        if let name = name {
+            self.nameLabel.text = name
+        }
         
     }
     @IBAction func backbtn(_ sender: UIButton) {
