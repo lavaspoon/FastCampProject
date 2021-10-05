51c6193f97bb87deeecdf60db212a244ffed9bf5
lavaspoon <lavaspoon@naver.com>
2021-10-05 23:55:50 +0900

    뷰컨 - 델리게이트 위임완료 후, UI 반영 작업

--- FastCampProject/FastCampProject/ViewController.swift
+++ FastCampProject/FastCampProject/ViewController.swift
@@ -7,12 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, LEDSettingDelegate{
+    
     @IBOutlet weak var contentsLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.contentsLabel.textColor = .yellow
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let settingViewController = segue.destination as? SettingViewController {
+            //delegate 위임
+            settingViewController.delegate = self
+        }
+    }
+    
+    func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor) {
+        if let text = text {
+            self.contentsLabel.text = text
+        }
+        self.contentsLabel.textColor = textColor
+        self.view.backgroundColor = backgroundColor
     }
 
 }
