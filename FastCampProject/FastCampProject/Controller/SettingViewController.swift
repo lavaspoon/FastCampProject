897e0b66f53b0825c3ee68ede61de59c805e31c2
lavaspoon <lavaspoon@naver.com>
2021-10-07 15:36:31 +0900

    2강 LED 프로젝트 완료! 및 주석작업

--- FastCampProject/FastCampProject/Controller/SettingViewController.swift
+++ FastCampProject/FastCampProject/Controller/SettingViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
-
+//1) 이전 화면으로 데이터 전달을 위한 프로토콜 선언
 protocol LEDSettingDelegate : AnyObject{
+    //설정값을 전달하는 함수 정의(이전화면으로 전달할 값)
     func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor)
 }
 class SettingViewController: UIViewController {
@@ -19,8 +20,11 @@ class SettingViewController: UIViewController {
     @IBOutlet weak var blackBtn: UIButton!
     @IBOutlet weak var blueBtn: UIButton!
     @IBOutlet weak var purpleBtn: UIButton!
+    
+    //2) 델리게이트 프로퍼티 추가
     weak var delegate : LEDSettingDelegate?
-    var ledText : String?
+    
+    var ledText : String? // 뷰 컨트롤러에서 설정된 텍스트를 가져오기 위한 프로퍼티
     var textColor : UIColor = .yellow
     var backgroundColor : UIColor = .black
     
@@ -28,16 +32,19 @@ class SettingViewController: UIViewController {
         super.viewDidLoad()
         self.configureView()
     }
-    
+    //MARK: 1-이전화면의 값들을 세팅뷰에서 받음
     private func configureView(){
         if let ledText = self.ledText {
             self.textField.text = ledText
         }
+        //이전화면에서 받은 컬러와 백그다운드를 change 함수의 파라미터로 전달해줌
         self.changeTextColor(color: self.textColor)
         self.changeBackGroundColor(color: self.backgroundColor)
     }
+    
     @IBAction func tapTextColorBtn(_ sender: UIButton) {
-        if sender == self.yellowBtn {
+        //sender 파라미터는 클릭한 버튼의 색을 담음
+        if sender == self.yellowBtn { //전달받은 sender 가 엘로우 버튼이면 텍스트 컬러를 엘로우로 바꿈
             self.changeTextColor(color: .yellow)
             self.textColor = .yellow
         } else if sender == self.orangeBtn {
@@ -59,9 +66,11 @@ class SettingViewController: UIViewController {
             self.changeBackGroundColor(color: .purple)
             self.backgroundColor = .purple
         }
+        
     }
     @IBAction func tapSaveBtn(_ sender: UIButton) {
         self.delegate?.changedSetting(
+            //설정된 프로퍼티 값들을 파라미터로 전달함
             text: self.textField.text,
             textColor: self.textColor,
             backgroundColor: self.backgroundColor
@@ -70,6 +79,7 @@ class SettingViewController: UIViewController {
     }
     
     private func changeTextColor(color : UIColor) {
+        //파라미터로 전달받은 색상이 해당색상이면 1, 아니면 0.5
         self.yellowBtn.alpha = color == UIColor.yellow ? 1 : 0.2
         self.orangeBtn.alpha = color == UIColor.orange ? 1 : 0.2
         self.greenBtn.alpha = color == UIColor.green ? 1 : 0.2
--- FastCampProject/FastCampProject/ViewController.swift
+++ FastCampProject/FastCampProject/ViewController.swift
@@ -14,18 +14,22 @@ class ViewController: UIViewController, LEDSettingDelegate{
         super.viewDidLoad()
         self.contentsLabel.textColor = .yellow
     }
-    
+    //3) 세그웨이로 화면전환 되니깐 prepare 함수를 통해서 값 받는다.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //다운캐스팅 하여 세팅뷰컨트롤러의 프로퍼티 접근
         if let settingViewController = segue.destination as? SettingViewController {
             //delegate 위임
             settingViewController.delegate = self
+            
+            //MARK: 2-세팅뷰로 현재 값들을 보내줌
             settingViewController.ledText = self.contentsLabel.text
             settingViewController.textColor = self.contentsLabel.textColor
             settingViewController.backgroundColor = self.view.backgroundColor ?? .black
         }
     }
-    
+    //4) 값을 받기위해 반드시 선언해줘야 함
     func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor) {
+        //이전화면에서 전달하는 설정값들을 뷰에 초기화 시켜줌
         if let text = text {
             self.contentsLabel.text = text
         }
