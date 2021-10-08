b19e7cb03d08d509528bbcb204a47c4a377279e2
lavaspoon <lavaspoon@naver.com>
2021-10-08 22:16:37 +0900

    복습 - 테스트 뷰 UI 완성 및 컨트롤러 연결

---
+++ FastCampProject/FastCampProject/Controller/TestViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TestViewController.swift
+//  FastCampProject
+//
+//  Created by lavaspoon on 2021/10/08.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
