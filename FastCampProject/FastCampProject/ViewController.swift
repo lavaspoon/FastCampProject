7522912441edb1b7f473bdac6ec7c9346174dc0b
lavaspoon <lavaspoon@naver.com>
2021-10-11 19:27:40 +0900

    에러수정 - 초기 뷰컨트롤러 설정

--- FastCampProject/FastCampProject/ViewController.swift
+++ FastCampProject/FastCampProject/ViewController.swift
@@ -7,10 +7,49 @@
 
 import UIKit
 
+enum Operation {
+    case Nanugi
+    case Gobhagi
+    case Minus
+    case Plus
+    case unKnown
+}
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var numberOutputLabel: UILabel!
+    
+    var displayNumber = ""
+    var firstOperand = ""
+    var secondOperand = ""
+    var result = ""
+    var currentOperation : Operation = .unKnown
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    @IBAction func tapDotButton(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapNumberButton(_ sender: UIButton) {
+        guard let numberValue = sender.title(for: .normal) else { return }
+        print(numberValue)
+    }
+    @IBAction func tapClearButton(_ sender: UIButton) {
+    
+    }
+    @IBAction func tapNanugiButton(_ sender: UIButton) {
+    
+    }
+    @IBAction func tapGobhagiButton(_ sender: UIButton) {
+    
+    }
+    @IBAction func tapPlusButton(_ sender: UIButton) {
+    
+    }
+    @IBAction func tapMinusButton(_ sender: UIButton) {
+    
+    }
+    @IBAction func tapEqualButton(_ sender: UIButton) {
+    
+    }
 }
