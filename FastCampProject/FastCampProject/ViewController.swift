d833ad97916758cf7df56718d7b9b69acd7e1c8d
lavaspoon <lavaspoon@naver.com>
2021-10-14 17:03:13 +0900

    4강 Todo List alert 기능 구현완료

--- FastCampProject/FastCampProject/ViewController.swift
+++ FastCampProject/FastCampProject/ViewController.swift
@@ -9,8 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "할 일 등록", message: nil, preferredStyle: .alert)
+        let registerButton = UIAlertAction(title: "등록", style: .default, handler: { _ in
+            //텍스트필드의 0번째 배열을 가져옴
+            debugPrint("\(alert.textFields?[0].text)")
+        })
+        let cancelButton = UIAlertAction(title: "취소", style: .default, handler: nil)
+        alert.addAction(registerButton)
+        alert.addAction(cancelButton)
+        alert.addTextField(configurationHandler: { inputText in
+            inputText.placeholder = "할 일을 입력해주세요."
+            
+        })
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
