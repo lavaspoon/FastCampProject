db990e24264e814b5ba907ed848a14e7f8c8e8cd
lavaspoon <lavaspoon@naver.com>
2021-10-14 20:39:28 +0900

    add todo 추가버튼 기능 구현 완료

---
+++ FastCampProject/FastCampProject/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  FastCampProject
+//
+//  Created by lavaspoon on 2021/10/14.
+//
+
+import Foundation
+
+struct Task {
+    var title : String
+    var done : Bool
+}
--- FastCampProject/FastCampProject/ViewController.swift
+++ FastCampProject/FastCampProject/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    var tasks = [Task]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.dataSource = self
+        
     }
     @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
     }
     @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "할 일 등록", message: nil, preferredStyle: .alert)
-        let registerButton = UIAlertAction(title: "등록", style: .default, handler: { _ in
+        let registerButton = UIAlertAction(title: "등록", style: .default, handler: { [weak self] _ in
             //텍스트필드의 0번째 배열을 가져옴
             debugPrint("\(alert.textFields?[0].text)")
+            
+            guard let title = alert.textFields?[0].text else { return }
+            let task = Task(title: title, done: false)
+            self?.tasks.append(task)
+            self?.tableView.reloadData()
         })
         let cancelButton = UIAlertAction(title: "취소", style: .default, handler: nil)
         alert.addAction(registerButton)
@@ -31,4 +40,17 @@ class ViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
+}
+
+extension ViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let task = self.tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        return cell
+    }
 }
