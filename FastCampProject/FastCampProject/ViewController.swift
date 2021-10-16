df888d9c41799d97e4178cd50fb13a22239e431e
lavaspoon <lavaspoon@naver.com>
2021-10-16 23:10:10 +0900

    추가 주석

--- FastCampProject/FastCampProject/ViewController.swift
+++ FastCampProject/FastCampProject/ViewController.swift
@@ -10,26 +10,30 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    //0) Task 구조체 생성자 생성
     var tasks = [Task]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //dataSource 채택
+        //0) dataSource 채택
         self.tableView.dataSource = self
         
     }
     @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+    
     }
     @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "할 일 등록", message: nil, preferredStyle: .alert)
         let registerButton = UIAlertAction(title: "등록", style: .default, handler: { [weak self] _ in
             //텍스트필드의 0번째 배열을 가져옴
             debugPrint("\(alert.textFields?[0].text)")
-            
+            //1) alert에 입력한 타이틀값을 title 변수에 담은 후
             guard let title = alert.textFields?[0].text else { return }
+            //2) Task 구조체에 담아 task 변수에 저장
             let task = Task(title: title, done: false)
-            //tasks 배열에 할일이 추가 됨
+            //3) tasks 배열에 할일이 추가
             self?.tasks.append(task)
+            //6) 등록버튼 클릭 시, 테이블뷰 갱신
             self?.tableView.reloadData()
         })
         let cancelButton = UIAlertAction(title: "취소", style: .default, handler: nil)
@@ -54,9 +58,9 @@ extension ViewController : UITableViewDataSource {
     }
     //cellForRowAt: 특정 섹션의 N번째 Row를 그리는데 필요한 셀을 반환하는 메서드
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //스토리보드에 정의한 셀을 가져옴
+        //4) 스토리보드에 정의한 셀을 가져옴
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        //배열에 저장된 할일 값을 가져옴
+        //5) 배열에 저장된 할일 값을 가져옴
         let task = self.tasks[indexPath.row]
         cell.textLabel?.text = task.title
         return cell
