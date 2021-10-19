84983f77199a773c36133bc184f13d38f7a0d731
lavaspoon <lavaspoon@naver.com>
2021-10-19 23:10:43 +0900

    Message 함수를 추가하여 주석 보강

--- FastCampProject/FastCampProject/ViewController.swift
+++ FastCampProject/FastCampProject/ViewController.swift
@@ -12,11 +12,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     //0) Task 구조체 생성자 생성
     var tasks = [Task]() {
+        //배열에 요소가 추가 될때 마다 실행
         didSet {
+            print("saveTasks 실행됨")
             self.saveTasks()
         }
     }
+    
+    var forms = [Task](){
+        //배열에 요소가 추가 될때 마다 실행
+        didSet{
+            print("saveMessage 실행됨")
+            self.saveMessage()
+        }
+    }
+    
     func saveTasks(){
+        //배열에 있는 요소를 딕셔너리 형태로 매핑
         let data = self.tasks.map {
             [
                 "title": $0.title,
@@ -29,15 +41,37 @@ class ViewController: UIViewController {
     //뷰컨트롤러 로드 했을떄 함수 실행
     func loadTasks(){
         let userDefaults = UserDefaults.standard
-        //object 메서드가 반환하는 값이 String 이라고 지정해주는 타입캐스팅
+        //object 메서드는 Any 타입으로 반환, 반환하는 값이 딕셔너리이기 때문에 타입캐스팅, 타입캐스팅이 실패할 수 도 있으니 guard문 처리
         //as? : 캐스팅 성공하면 옵셔널값 반환하고 실패 하면 nil 반환
         guard let data = userDefaults.object(forKey: "tasks") as? [[String: Any]] else { return }
         //compactMap : 1차원 배열에서 nil 제거하고, 옵셔널 바인딩 할때 사용
         //as? : 캐스팅 성공하면 옵셔널값 반환하고 실패 하면 nil 반환
-        self.tasks = data.compactMap {
+        self.tasks = data.compactMap { //다시 tasks 배열에 저장 compactMap으로 매핑
+            //딕셔너리 value 가 Any 타입이니깐 String, Bool 로 타입 캐스팅
             guard let title = $0["title"] as? String else { return nil }
             guard let done = $0["done"] as? Bool else { return nil }
             return Task(title: title, done: done)
+        }
+    }
+    //배열에 요소 추가할때마다 실행할 함수
+    func saveMessage(){
+        let message = self.forms.map {
+            [   //딕셔너리 형태로 form 배열에 저장
+                "message": $0.title,
+                "button": $0.done
+            ]
+        }
+        let UD = UserDefaults.standard
+        UD.set(message, forKey: "forms")
+    }
+    func loadMessage(){
+        let UD = UserDefaults.standard
+        guard let data = UD.object(forKey: "forms") as? [[String: Any]] else { return }
+        //다시 배열에 넣어줌
+        self.forms = data.compactMap{
+            guard let message = $0["message"] as? String else { return nil }
+            guard let button = $0["button"] as? Bool else { return nil }
+            return Task(title: message , done: button)
         }
     }
     
@@ -45,7 +79,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         //0) dataSource 채택
         self.tableView.dataSource = self
-        self.loadTasks()
+        //self.loadTasks()
+        self.loadMessage()
     }
     @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
         //edit
@@ -61,6 +96,7 @@ class ViewController: UIViewController {
             let task = Task(title: title, done: false)
             //3) tasks 배열에 할일이 추가
             self?.tasks.append(task)
+            self?.forms.append(task)
             //6) 등록버튼 클릭 시, 테이블뷰 갱신
             self?.tableView.reloadData()
         })
