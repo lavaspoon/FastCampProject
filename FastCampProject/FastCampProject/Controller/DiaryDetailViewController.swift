22a5f83820fd6c6e08ff358a873f582a60e1c47c
lavaspoon <lavaspoon@naver.com>
2021-10-22 00:22:59 +0900

    상세 글 뷰 UI 작성

---
+++ FastCampProject/FastCampProject/Controller/DiaryDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DiaryDetailViewController.swift
+//  FastCampProject
+//
+//  Created by lavaspoon on 2021/10/21.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
