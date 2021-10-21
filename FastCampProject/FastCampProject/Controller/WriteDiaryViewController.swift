c3993421b9d5b5e0e82ac174a95f59b7816ca84b
lavaspoon <lavaspoon@naver.com>
2021-10-21 23:57:03 +0900

    일기장 작성 UI 완료

---
+++ FastCampProject/FastCampProject/Controller/WriteDiaryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WriteDiaryViewController.swift
+//  FastCampProject
+//
+//  Created by lavaspoon on 2021/10/21.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
